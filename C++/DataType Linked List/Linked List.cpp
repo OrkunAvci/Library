de7eaa397586dcf91a02e3e40a41ba1dba2be8a4
Orkun Avcı <orkunavci28@gmail.com>
2020-12-28 15:25:58 +0300

    Intermediate commit

---
+++ C++/DataType Linked List/Linked List.cpp
@@ -0,0 +1,23 @@
+class LinkedList
+{
+private:
+    struct NODE
+    {
+        int data;
+        struct NODE* next;
+    };
+    struct NODE* head;
+    struct NDOE* tail;
+    
+public:
+    LinkedList()
+    {
+
+    }
+
+    ~LinkedList()
+    {
+
+    }
+
+};
