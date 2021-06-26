883f51e5b797f6ed2c8664d6dd6b152c584277d0
OrkunAvci <orkunavci28@gmail.com>
2021-06-26 16:29:49 +0300

    Session 1 Init

---
+++ Phyton/Learning/Session 1/Variables.py
@@ -0,0 +1,13 @@
+var1 = 20
+var2 = "Text"
+var3 = 1.5
+var4 = []
+var5 = {}
+var6 = ()
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
+print(type(var5))
+print(type(var6))
