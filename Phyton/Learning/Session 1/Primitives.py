7944af82e3d95a6a67f9ebd6890aab59e5641ad0
OrkunAvci <orkunavci28@gmail.com>
2021-06-27 13:45:13 +0300

    Create Primitives.py

---
+++ Phyton/Learning/Session 1/Primitives.py
@@ -0,0 +1,27 @@
+#	Boolean
+bool_var1 = True
+bool_var2 = False
+print(bool_var1 == bool_var2)
+print(bool_var1 != bool_var2)
+print(not bool_var2)
+
+#	Int
+int_var1 = 200
+int_var2 = 100
+print(int_var1 + int_var2)
+print(int_var1 - int_var2)
+print(int_var1 * int_var2)
+print(int_var1 / int_var2)
+print(int_var1 % int_var2)
+
+#	Float
+float_var1 = 65.0
+float_var2 = 15.0
+print(float_var1 + float_var2)
+print(float_var1 - float_var2)
+print(float_var1 * float_var2)
+print(float_var1 / float_var2)
+print(float_var1 % float_var2)
+
+#	String
+#	No string code
