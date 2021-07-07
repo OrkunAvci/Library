5802e98ac1322f97959e5993119df057c8f24247
OrkunAvci <orkunavci28@gmail.com>
2021-07-08 00:59:38 +0300

    Update Higher Order Functions.py

--- Phyton/Learning/Session 3/Higher Order Functions.py
+++ Phyton/Learning/Session 3/Higher Order Functions.py
@@ -1,15 +1,15 @@
-def shout():
-	return "This is returned from a function.".upper()
+def shout(*args):
+	return f"This is returned from a function {args[0]}.".upper()
 
 
-def whisper():
+def whisper(*args):
 	return "This is returned from a function.".lower()
 
 
-def greet(get_str):
-	greeting = get_str()
+def greet(*args):
+	greeting = args[0](args[1:])
 	print(greeting)
 
 
-greet(shout)
+greet(shout, "text")
 greet(whisper)
