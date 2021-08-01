929321a8878ef608157f11f408c4add40e3e827f
OrkunAvci <orkunavci28@gmail.com>
2021-08-01 19:52:06 +0300

    Update Password Generator.py

--- Phyton/Password Generator/Password Generator.py
+++ Phyton/Password Generator/Password Generator.py
@@ -1,19 +1,20 @@
 import random as rand
 import string as str
 
-accepted_characters = str.ascii_lowercase + str.ascii_uppercase + str.digits
+if __name__ == "__main__":
+	accepted_characters = str.ascii_lowercase + str.ascii_uppercase + str.digits
 
-request = input("Add extra fuckery? (y) or (n)")
-if (request == "y"):
-	accepted_characters += str.punctuation
+	request = input("Add extra fuckery? (y) or (n)")
+	if (request == "y"):
+		accepted_characters += str.punctuation
 
-rand_max = len(accepted_characters)
+	rand_max = len(accepted_characters)
 
-request = input("Give me the length: ")
-request = int(request)
+	request = input("Give me the length: ")
+	request = int(request)
 
-out = ""
-for i in list(range(request)):
-	out += accepted_characters[ rand.randint(-1, rand_max) ]
+	out = ""
+	for i in list(range(request)):
+		out += accepted_characters[ rand.randint(-1, rand_max) ]
 
-print(out)
+	print(out)
